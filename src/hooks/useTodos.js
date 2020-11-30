1d7efe5313168ec004d28ce250478cdf6e6e55b8
v61265 <v61265@gmail.com>
2020-11-30 22:21:49 +0800

    fix localStorage JSON

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -11,11 +11,8 @@ export default function useTodos() {
   const id = useRef(1);
 
   const [todos, setTodos] = useState(() => {
-    let temp = window.localStorage.getItem("todos") || [
-      { id: 2, content: "送出履歷", isDone: false, isEditing: false },
-      { id: 1, content: "寫履歷", isDone: true, isEditing: false },
-    ];
-    console.log("hi");
+    let temp = window.localStorage.getItem("todos") || "[{"id":2,"content":"送出履歷","isDone":false,"isEditing":false},{"id":1,"content":"寫履歷","isDone":true,"isEditing":false}]";
+    console.log(temp);
     const todoData = JSON.parse(temp);
     id.current = todoData[0] ? todoData[0].id + 1 : 1;
 
